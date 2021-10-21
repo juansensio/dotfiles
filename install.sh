0e19a4b8293752062e85cf9d0df1fc467da3d7ef
Juan Sensio <sensio.juan@gmail.com>
2021-10-21 10:45:00 +0200

    update

---
+++ install.sh
@@ -0,0 +1,7 @@
+# install oh my zshrc
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# install oh my zshrc plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+# copy dotfiles
+# TODO
