b180163d999fbb9c754f20df76b8da8acf9e1353
Juan Sensio <it@earthpulse.es>
2023-03-24 16:33:36 +0100

    update

---
+++ pytorch.sh
@@ -0,0 +1,15 @@
+# data science libs
+
+pip install -y numpy pandas matplotlib jupyter scikit-learn scikit-image
+
+# install pytorch with gpu support
+# https://pytorch.org/ 
+
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+
+# install pytorch lightning
+
+pip install lightning torchmetrics
+
+
+
